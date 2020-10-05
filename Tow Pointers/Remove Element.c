703114a3f852b8fbb013bd5e6f564446906e11d6
AhmedElbanawi <elbanawiahmed@yahoo.com>
2020-10-06 00:28:27 +0200

    Update Remove Element.c

--- Tow Pointers/Remove Element.c
+++ Tow Pointers/Remove Element.c
@@ -1,3 +1,4 @@
+![psttt](https://user-images.githubusercontent.com/51443318/95138504-aae51600-076a-11eb-97dc-51a36a8c9f87.png)
 int removeElement(int* nums, int numsSize, int val){
     int i, j;
     for(i = 0, j = 0; j < numsSize; j++){
